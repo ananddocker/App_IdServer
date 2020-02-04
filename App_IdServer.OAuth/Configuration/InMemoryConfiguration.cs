7ee520a44d168dc53651b21f189e0305e67a2b36
Anand Gangadhara <anandgangadhara@neemconsulting.com>
2020-02-04 14:24:37 +0000

    Add project files.

---
+++ App_IdServer.OAuth/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace App_IdServer.OAuth.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[] { new ApiResource("LoanApplication", "LoanApplication") };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[] { new Client {
+                ClientId="anand",
+                ClientSecrets= new[] {new Secret("secret".Sha256())},
+                AllowedGrantTypes=GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                AllowedScopes=new[]{"LoanApplication"}
+            } };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[] {new TestUser
+            {
+                SubjectId="1",
+                Password="password",
+                Username="anand.gangadhara@hotmail.com"
+            }};
+        }
+    }
+}
