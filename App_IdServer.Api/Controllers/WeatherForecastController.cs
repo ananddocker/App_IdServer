c91054a105b04815ab9bea8f1d32c4661cbed13b
Anand Gangadhara <anandgangadhara@neemconsulting.com>
2020-02-04 14:27:59 +0000

    Test Commit

--- App_IdServer.Api/Controllers/WeatherForecastController.cs
+++ App_IdServer.Api/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace App_IdServer.Api.Controllers
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
